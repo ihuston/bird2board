c33bc9968e1627401d4ddcf9eaf7a9cbb0a121e3
Ian Huston <ian@ianhuston.net>
2021-06-02 20:23:06 +0100

    Close #2. Pull relevant tweet data out of JSON dump.

--- bird2board/bird2board.py
+++ bird2board/bird2board.py
@@ -32,14 +32,20 @@ class Twitter:
 
         for t in tweets:
             tweet_data = t['content']['itemContent']['tweet']
-            parsed_tweet = {"screen_name": tweet_data['core']['user']['legacy']['screen_name'],
+            screen_name = tweet_data['core']['user']['legacy']['screen_name']
+            rest_id = tweet_data['rest_id']
+            parsed_tweet = {"screen_name": screen_name,
                             "full_text": tweet_data['legacy']['full_text'],
-                            "rest_id": tweet_data['rest_id']}
+                            "rest_id": rest_id,
+                            "tweet_url": self.tweet_url(screen_name, rest_id)}
             if len(tweet_data['legacy']['entities']['urls']) > 0:
                 parsed_tweet["expanded_url"] = tweet_data['legacy']['entities']['urls'][0]['expanded_url']
             extracted_data.append(parsed_tweet)
 
         return extracted_data
+
+    def tweet_url(self, screen_name, rest_id):
+        return f"https://twitter.com/{screen_name}/status/{rest_id}"
 
 
 def main():
--- bird2board/test_twitterparse.py
+++ bird2board/test_twitterparse.py
@@ -44,10 +44,40 @@ class TestTwitterBookmarksParse(TestCase):
 
         expected = [{"screen_name": "my_name", "rest_id": 1,
                      "full_text": "my tweet", "expanded_url": "my_url",
-                     },
+                     "tweet_url": "https://twitter.com/my_name/status/1"},
                     {"screen_name": "my_other_name", "rest_id": 2,
-                     "full_text": "my other tweet", "expanded_url": "my_other_url"}]
+                     "full_text": "my other tweet", "expanded_url": "my_other_url",
+                     "tweet_url": "https://twitter.com/my_other_name/status/2"}]
+
         twitter = bird2board.Twitter()
         result = twitter.parse_json(test_json_text)
         self.assertDictEqual(result[0], expected[0])
         self.assertDictEqual(result[1], expected[1])
+
+    def test_no_urls(self):
+        t1 = {'content':
+                  {'itemContent':
+                       {'tweet':
+                            {'legacy':
+                                 {'full_text':
+                                      "my tweet",
+                                  'entities': {'urls': []}
+                                  },
+                             'core':
+                                 {'user': {'legacy': {'screen_name': "my_name"}}},
+                             'rest_id': 1
+                             }
+                        }
+                   }
+              }
+
+        test_json_dict = {'data': {'bookmark_timeline': {'timeline': {'instructions': [{'entries': [t1]}]}}}}
+        test_json_text = json.dumps(test_json_dict)
+
+        expected = [{"screen_name": "my_name", "rest_id": 1,
+                     "full_text": "my tweet",
+                     "tweet_url": "https://twitter.com/my_name/status/1"}]
+
+        twitter = bird2board.Twitter()
+        result = twitter.parse_json(test_json_text)
+        self.assertDictEqual(result[0], expected[0])
