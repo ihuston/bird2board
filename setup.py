a48ba9f3c75a820e34710f97d4358cba551b98f2
Ian Huston <ian@ianhuston.net>
2021-06-13 23:25:13 +0100

    WIP #11. Detect Github Actions workflow and create unique version for Test PyPI.

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
+import os
+
 from setuptools import setup
 from os import path
 
+version = '0.1.0'
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+if "GITHUB_WORKFLOW" in os.environ:
+    print(f"Running inside {os.getenv('GITHUB_WORKFLOW')}")
+    if os.getenv('GITHUB_WORKFLOW') == "Upload Python Package on Test PyPI":
+        version = os.getenv("GITHUB_SHA")
+
 
 setup(
     name='bird2board',
-    version='0.1.0',
+    version=version,
     packages=['bird2board'],
     url='https://github.com/ihuston/bird2board',
     license='MIT',
     author='Ian Huston',
     author_email='ian@ianhuston.net',
+    description='Convert bird based bookmarks to board based ones.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     entry_points={
