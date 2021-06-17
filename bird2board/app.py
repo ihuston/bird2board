5747c9378f858e1fbd2e4805e253d79f51c09ef4
Ian Huston <ian@ianhuston.net>
2021-06-17 20:54:52 +0100

    Make sure that environment variable for token is available when invoked from script instead of module.

--- bird2board/app.py
+++ bird2board/app.py
@@ -12,7 +12,8 @@ from bird2board.bird2board import Bird2Board
 @click.option('--toread/--not-toread', default=False, help='set Pinboard bookmarks as "to read"')
 @click.option('--shared/--not-shared', default=False, help='set Pinboard bookmarks as shared')
 @click.option('--replace/--no-replace', default=False, help='replace existing Pinboard bookmark for an URL')
-@click.option('-p', '--pinboard-token', required=True, help='user token for Pinboard API')
+@click.option('-p', '--pinboard-token', required=True, help='user token for Pinboard API',
+              envvar="BIRD2BOARD_PINBOARD_TOKEN")
 @click.argument('path', type=click.Path(exists=True, path_type=pathlib.Path, dir_okay=True, file_okay=True))
 def convert(path, pinboard_token, replace, shared, toread, debug):
     """Save Twitter Bookmark .json file(s) at PATH (file or directory) to account using PINBOARD TOKEN."""
